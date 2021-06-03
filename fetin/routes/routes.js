b14ed64e4380be48b8f708ba76155001c8d39089
gabrielss2406 <59343269+gabrielss2406@users.noreply.github.com>
2021-06-03 12:23:51 -0300

    Update routes.js

--- fetin/routes/routes.js
+++ fetin/routes/routes.js
@@ -3,16 +3,20 @@
     const router = express.Router();
 
     const UserController = require('../controllers/UserController');
+    const PerfilController = require('../controllers/PerfilController');
+    const BuscaController = require('../controllers/BuscaController');
 
+    const verifyJWT = require("../helpers/verifyJWT")
 // Rotas
-    router.get(("/"),(req,res) =>{
-        res.json({
-            numero: 11,
-            texto: "Hello World!"
-        });
-    });
+    router.get(("/"),(req,res) =>{res.json({numero: 11,texto: "Hello World!"});});
 
     router.post("/registrar", UserController.register);
     router.post("/login", UserController.login)
+    router.post("/logout", UserController.logout)
+
+    router.get("/perfil", PerfilController.index);
+    router.post("/perfil/edit", verifyJWT ,PerfilController.edit);
+
+    router.post("/buscar",verifyJWT,BuscaController.buscar)
 
 module.exports = router;
