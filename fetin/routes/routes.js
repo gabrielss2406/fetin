d9cdce7de31a0301f1de08e2b14640152a9d1140
Francisco-Pereira-Guimaraes <84347904+Francisco-Pereira-Guimaraes@users.noreply.github.com>
2021-06-08 09:00:01 -0300

    Routes
    Rotas de senha adicionadas

--- fetin/routes/routes.js
+++ fetin/routes/routes.js
@@ -1,22 +1,27 @@
-// Carregando Módulos
-    const express = require("express");
-    const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-    const UserController = require('../controllers/UserController');
-    const PerfilController = require('../controllers/PerfilController');
-    const BuscaController = require('../controllers/BuscaController');
+const UserController = require('../controllers/UserController');
+const PerfilController = require('../controllers/PerfilController');
+const BuscaController = require('../controllers/BuscaController');
+const PasswordController = require('../controllers/PasswordController');
 
-    const verifyJWT = require("../helpers/verifyJWT")
+const verifyJWT = require("../helpers/verifyJWT")
 // Rotas
-    router.get(("/"),(req,res) =>{res.json({numero: 11,texto: "Hello World!"});});
+router.get(("/"),(req,res) =>{res.json({numero: 11,texto: "Hello World!"});});
 
-    router.post("/registrar", UserController.register);
-    router.post("/login", UserController.login)
-    router.post("/logout", UserController.logout)
+router.post("/registrar", UserController.register);
+router.post("/login", UserController.login)
+router.post("/logout", UserController.logout)
 
-    router.get("/perfil", PerfilController.index);
-    router.post("/perfil/edit", verifyJWT ,PerfilController.edit);
+router.post("/recuperarSenha", PasswordController.forgotPass)
+router.get("/reset", PasswordController.reset)
+router.post("/reset", PasswordController.nova_senha)
+router.post("/editarSenha", PasswordController.editSenha)
 
-    router.post("/buscar",verifyJWT,BuscaController.buscar)
+router.get("/perfil", PerfilController.index);
+router.post("/perfil/edit", verifyJWT ,PerfilController.edit);
+
+router.post("/buscar",verifyJWT,BuscaController.buscar)
 
 module.exports = router;
