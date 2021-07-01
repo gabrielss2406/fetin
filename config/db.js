3adfd9b7aa8289f7fa8621c78c232b1d5f2651cb
Francisco-Pereira-Guimaraes <84347904+Francisco-Pereira-Guimaraes@users.noreply.github.com>
2021-07-01 11:26:32 -0300

    Config do mongo

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb://localhost/fetin"} //BD Produção
+    module.exports = {mongoURI: "mongodb+srv://Werk:werksd21@werksd.rualo.mongodb.net/Werk"} //BD Produção
 }else{
-    module.exports = {mongoURI: "mongodb://localhost/fetin"} //BD Teste Local
+    module.exports = {mongoURI: "mongodb+srv://Werk:werksd21@werksd.rualo.mongodb.net/Werk"} //BD Teste Local
 }
