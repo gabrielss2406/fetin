d69c238f60cf52f8ec50493c9ce4278fd09acb0b
gabrielss2406 <59343269+gabrielss2406@users.noreply.github.com>
2021-07-01 11:27:43 -0300

    Delete fetin directory

--- fetin/README.txt
+++
@@ -1,7 +0,0 @@
-Para configurar o Secret do Token, insira os seguintes arquivos:
-
-.env.example
-SECRET=
-
-.env
-SECRET=mysecret
--- fetin/config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb://localhost/fetin"} //BD Produção
-}else{
-    module.exports = {mongoURI: "mongodb://localhost/fetin"} //BD Teste Local
-}
